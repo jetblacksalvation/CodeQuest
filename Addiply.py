9ba2e2601802b66ff93da225110a3a71df29e288
Xx_JetBlackSalvation_xX <91695842+jetblacksalvation@users.noreply.github.com>
2023-02-14 17:55:06 -0700

    Update Addiply.py

--- Addiply.py
+++ Addiply.py
@@ -4,5 +4,5 @@ length = int(input())
 
 for x in range(length):
     arr = list(*map(lambda x,y: (int(x)+int(y) , int(y)*int(x)), *input().rstrip().split()))
-    print(*arr, sep=" ")
+    print(*arr)
     pass
